c39b3a4ed6dc40facc8fa867732dee72c36aa60c
Andersen <actfire@gmail.com>
2023-11-15 20:27:37 +0800

    u

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Home from './components/Home.vue';
+import Test from './components/Test.vue';
 </script>
 
 <template>
   <Home />
+  <hr>
+  <Test />
 </template>
 
 <style scoped>
---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const value1 = ref(true)
+</script>
+
+<template>
+  <el-switch v-model="value1" />
+</template>
+
+<style scoped>
+</style>
